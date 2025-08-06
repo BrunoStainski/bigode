0e9a28c1d18579cbf1ca1c7a5da0b6013d7cb975
BrunoStainski <brunoparentestainski@gmail.com>
2025-08-06 20:20:16 -0300

    Add files via upload

---
+++ progamatop.c
@@ -0,0 +1,16 @@
+//BIBLIOTECA QUE IRÁ MANIPULAR A ENTRADA DE SAÍDA DE DADOS
+#include <stdio.h>
+#include <stdlib.h>
+
+//MÉTODO MAIN PARA EXECUÇÃO 
+int main () {
+    //CÓDIGO A SER DESENVOLVIDO 
+ 
+  // \n COMANDO PARA PULAR UMA LINHA NO PRINT 
+    printf ("Hello, World!\n");
+    
+    system ("pause");
+
+    //RETORNO DA MINHA FUNÇÃO 
+    return 0;
+} 
